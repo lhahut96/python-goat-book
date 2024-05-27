3d5cfd67fb381f4c72ee93a06e49eeb62d757691
Lucas <lhahut96@gmail.com>
2024-05-26 21:31:42 -0700

    Add app for lists, with deliberately failing unit test

---
+++ README.md
@@ -0,0 +1,17 @@
+# Goat-Book
+
+## Description
+
+This is a Goat-book based project for my learning but using all the latest libs
+
+## Table of Contents
+
+- [Installation](#installation)
+
+## Installation
+
+To install the required libraries, you can use pip, the package installer for Python. Open a terminal or command prompt and run the following command:
+
+```
+pip install -r requirements.txt
+```
--- functional_tests.py
+++ functional_tests.py
@@ -3,28 +3,28 @@ import unittest
 from selenium import webdriver
 
 
-class NewVisitorTest(unittest.TestCase):  
-    def setUp(self):  
-        self.browser = webdriver.Chrome()  
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_todo_list(self):  
+    def test_can_start_a_todo_list(self):
         # Edith has heard about a cool new online to-do app.
         # She goes to check out its homepage
-        self.browser.get("http://localhost:8000")  
+        self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
-        self.assertIn("To-Do", self.browser.title)  
+        self.assertIn("To-Do", self.browser.title)
 
         # She is invited to enter a to-do item straight away
-        self.fail("Finish the test!")  
+        self.fail("Finish the test!")
 
         [...]
 
         # Satisfied, she goes back to sleep
 
 
-if __name__ == "__main__":  
-    unittest.main()  
+if __name__ == "__main__":
+    unittest.main()
