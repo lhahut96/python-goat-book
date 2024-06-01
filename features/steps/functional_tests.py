42408eef3dc98f1a8704986ebe58b67e7bb50cc0
Tu Le <tu.le@designerjourneys.com>
2024-05-31 21:18:17 -0700

    Testing by ID and tag name

--- features/steps/functional_tests.py
+++ features/steps/functional_tests.py
@@ -1,5 +1,9 @@
+import time
+
 from behave import *
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 @given("Init webdriver chrome")
@@ -12,6 +16,11 @@ def step_impl(context):
     context.browser.get("http://localhost:8000")
     print("title " + context.browser.title)
     assert "To-Do" in context.browser.title
+    header_text = context.browser.find_element(By.TAG_NAME, "h1").text
+    assert "To-Do" in header_text
+
+    inputbox = context.browser.find_element(By.ID, "id_new_item")
+    assert inputbox.get_attribute("placeholder") == "Enter a to-do item"
 
 
 def after_all(context):
